e4ef1770e75593c9b89601bbf73a5eb725aef472
ryo28 <wakisan1@ezweb.ne.jp>
2023-09-22 01:28:41 +0900

    ✨ tailwindcssをカスタマイズしました

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        LightYellow: "#FDD378",
+      },
+      screens: {
+        xs: "500px",
+      },
+    },
   },
   plugins: [],
 };
