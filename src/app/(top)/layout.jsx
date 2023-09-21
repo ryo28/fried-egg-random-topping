117cc7a6c2617c42054255b5d8c371f21929807b
ryo28 <wakisan1@ezweb.ne.jp>
2023-09-22 01:39:18 +0900

    ✨ footerを作成しました

--- src/app/(top)/layout.jsx
+++ src/app/(top)/layout.jsx
@@ -1,3 +1,12 @@
 export default function TopLayout({ children }) {
-  return <main>{children}</main>;
+  return (
+    <>
+      <main>{children}</main>
+      <div className="flex justify-center">
+        <footer className="absolute bottom-2 text-amber-800">
+          © 2013 Waki
+        </footer>
+      </div>
+    </>
+  );
 }
