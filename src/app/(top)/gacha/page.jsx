5430ded9c72d730ded285b8a2fcbee835691f621
ryo28 <wakisan1@ezweb.ne.jp>
2023-10-06 20:57:27 +0900

    ✨ ガチャページを作成しました

--- src/app/(top)/gacha/page.jsx
+++ src/app/(top)/gacha/page.jsx
@@ -13,19 +13,18 @@ export default function Gacha() {
   return (
     <div className="flex h-[100dvh] flex-col items-center overflow-hidden bg-LightOrange">
       <div className="relative">
+        <div className="absolute z-50 h-[30rem] w-full select-none xs:h-52"></div>
         <Image
-          className="h-96 select-none xs:h-52 xs:w-96"
+          className="h-96 xs:h-52 xs:w-96"
           src={background}
           priority
           alt="background"
         />
-        <div
-          className={`absolute right-32 top-1/2 w-16 select-none xs:right-44 xs:w-10`}
-        >
+        <div className={`absolute right-32 top-1/2 w-16 xs:right-44 xs:w-10`}>
           <EggItem state={eggSwitch} />
         </div>
         <Image
-          className="absolute bottom-4 left-11 z-40 h-96 w-96 select-none xs:h-52 xs:w-80"
+          className="absolute bottom-4 left-11 z-10 h-96 w-96 xs:h-52 xs:w-80"
           src={chicken}
           priority
           alt="chicken"
