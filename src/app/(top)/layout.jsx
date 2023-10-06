c41c5c1257e97f63d8cc8c227cdd6d0d16de6388
ryo28 <wakisan1@ezweb.ne.jp>
2023-10-06 20:36:10 +0900

    ✨ 文字を選択できなくしました

--- src/app/(top)/layout.jsx
+++ src/app/(top)/layout.jsx
@@ -3,7 +3,7 @@ export default function TopLayout({ children }) {
     <>
       <main>{children}</main>
       <div className="flex justify-center">
-        <footer className="absolute bottom-2 text-amber-800">
+        <footer className="absolute bottom-2 text-amber-800 select-none">
           © 2023 Waki
         </footer>
       </div>
