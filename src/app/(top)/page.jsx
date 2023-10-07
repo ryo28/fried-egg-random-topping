c08510c45a72ab1d1d4a1868d8168b143ed6256d
ryo28 <wakisan1@ezweb.ne.jp>
2023-10-07 10:29:58 +0900

    ✨ 画像を取得できなくしました、テキストを選択できなくしました

--- src/app/(top)/page.jsx
+++ src/app/(top)/page.jsx
@@ -5,7 +5,8 @@ import topBig from "/public/topImageBig.webp";
 
 export default function Home() {
   return (
-    <div className=" h-[100dvh] bg-LightYellow">
+    <div className="h-[100dvh] bg-LightYellow">
+      <div className="absolute z-40 h-full w-full"></div>
       <div className="flex justify-center">
         <Image
           className="absolute h-96 w-screen xs:hidden"
@@ -14,7 +15,7 @@ export default function Home() {
           alt="目玉焼き"
         />
         <Image
-          className="absolute top-20  hidden  xs:top-2 xs:block  xs:w-96"
+          className="absolute top-20 hidden xs:top-2 xs:block xs:w-96"
           src={topSmall}
           priority
           alt="目玉焼き"
@@ -23,7 +24,7 @@ export default function Home() {
       <div className="flex justify-center">
         <div className="absolute bottom-1/4 xs:bottom-12">
           <Link
-            className="bg-orange-600 p-5 text-2xl hover:bg-orange-800 xs:p-2"
+            className="relative z-50 select-none bg-orange-600 p-5 text-2xl hover:bg-orange-800 xs:p-2"
             href="/gacha"
           >
             トッピングガチャを引く
